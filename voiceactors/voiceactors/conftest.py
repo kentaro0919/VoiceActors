276f97a16a15974d9cc96c00477c780b304b7dc5
Kentaro Matsuzaki <kentaro@KentaronoiMac.local>
2021-03-12 10:36:39 +0900

    first commit

---
+++ voiceactors/voiceactors/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from voiceactors.users.models import User
+from voiceactors.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
